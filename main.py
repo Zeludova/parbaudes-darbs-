ee8dd285c393ea5866cb3c7507f25b4a6a093478
jzeludova <91866214+Zeludova@users.noreply.github.com>
2022-05-02 06:40:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def ribs(*args, **kwargs):
+  first, second = args[0], args[1]
+  if first**3 == second**3:
+    return "equal"
+  
+  return first if first**3 > second**3 else second
+
+if __name__ == "__main__":
+  print(ribs(5, 5))
